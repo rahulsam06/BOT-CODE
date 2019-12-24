b4e0ab7a86f8279b1c12230d475b8a790f47e839
rahulsam06 <59195865+rahulsam06@users.noreply.github.com>
2019-12-24 14:23:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app =Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/sms",methods=['POST'])
+def sms_reply():
+
+    msg=request.form.get('Body')
+
+    resp=MessagingResponse()
+    resp.message("yes")
+    return str(resp)
+if __name__=="__main__":
+    app.run(debug=True)
+    
