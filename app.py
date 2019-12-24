d6942317bd23cd2b1ea50330f222a0ab00cacbe0
rahulsam06 <59195865+rahulsam06@users.noreply.github.com>
2019-12-24 14:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def sms_reply():
     msg=request.form.get('Body')
 
     resp=MessagingResponse()
-    resp.message(f"you said : {msg}")
+    resp.message(f"you sent msg to me : {msg}")
     return str(resp)
 if __name__=="__main__":
     app.run(debug=True)
